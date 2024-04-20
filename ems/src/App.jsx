c4d0c752962ce39642d3a5c4d1425d373760bcf2
Asritha29 <Singareddy99ashritha@gmail.com>
2024-04-20 18:00:49 +0530

    update

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
 import Signup from "./components/signup";
 import Dashboard from "./components/dashboard";
+import Layout from './components/layout';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,10 +13,11 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Layout />}>
           <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
